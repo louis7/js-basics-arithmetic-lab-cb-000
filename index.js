edfd5f21833ce1999d35cfd616ab0244a223fff8
louis baptiste <louisvis@ymail.com>
2020-01-20 13:56:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,4 +7,9 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
-let newID = 1000000078;
+const var1 = 2
+const var2 = 31
+const multiply = var1 * var2
+const random = Math.floor(Math.random() * 1000 ) + 1
+const mod = 16 % 6
+const max = Math.max(5, 10, 15, 20)
